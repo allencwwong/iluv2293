1a10af87fcf07402f22d929d7da0f8a3b0a6f4cc
allen wong <allencwwong@verizonmedia.com>
2019-10-12 15:54:58 -0700

    added onclick function to cta

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,27 @@
-$('main').load("home.html");
+
+var il2293 = (function(){
+        var init = function(){
+            $('.card.card-game').on('click',function(){
+                pageLoader('detail');
+            })
+            $('.card.card-hunt a').on('click', function () {
+                if ($(this).data('cta-info')){
+                    alert($(this).data('cta-info'));
+                }else{
+                    alert($(this).data('cta-book'));
+                }
+            })
+        }
+    
+        function pageLoader(page){
+            page = page + '.html';
+            $('main').html('').load(page);
+        }
+    
+        return {
+            init: init,
+        }
+})();
+
+console.log(il2293)
+il2293.init();
