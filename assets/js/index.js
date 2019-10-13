33938538ef91ac7aad30239213199ac4eaa09d86
allen wong <allencwwong@verizonmedia.com>
2019-10-13 14:40:33 -0700

    updating info template css

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ var content = {
             player: 'For 4 - 10 Players',
             team: 'Form 2 - 3 Teams',
             gameDur: 'Game duration: approx 120 - 160 mins',
-            location: 'popular city locations'
+            location: 'Popular city locations'
         },
         cta: '/pages/book/detail.html?id=1',
         imgs: {
@@ -34,7 +34,7 @@ var content = {
             player: 'For 4 - 12 Players',
             team: 'Form 2 - 4 Teams',
             gameDur: 'Game duration: approx 160 - 180 mins',
-            location: 'sf city wide'
+            location: 'SF city wide'
         },
         cta: '/pages/book/detail.html?id=2',
         imgs: {
@@ -47,16 +47,17 @@ var content = {
     "3": {
         hero: {
             url: 'both-bg.png',
-            heading: 'battle of the hunt',
+            heading: 'Pink Panther',
             desc: 'City Hunt most popular event creations is a fun and interactive treasure hunt in the WHOLE city! Exploring interesting bits of history and culture from Chinatown , North beach to Downtown Market.'
         },
         content: {
-            desc: 'how 2 play?',
+            desc: '<h4>Background</h4><p>Multiple sources have confirm the Pink Panther has been stolen and hidden somewhere in the Golden Gate Park. The first team to retrieve Pink Panther will win the Leigon of Honur and the title Champion of the Hunt!</p><h4>Overview</h4><p>3 - 4 Teams will compete against time and each others to locate the treasure. Teams have to be selected and confirm within 24 hours after booking. Starting location will be determine by a pre-hunt exercise!</p><h4>Rules</h4><p> Locate the treasure within 160 mins</p><p>Hints are available every 20mins if check point not achieved</p><p>Feel free to use any devices for help but most clues does not require any extra research</p>',
             difficulty: '4',
-            player: '6 - 20',
-            team: '3 - 4',
+            player: 'For 6 - 20 Players',
+            team: 'Form 3 - 4 Teams',
             gameDur: 'Game duration: 160 mins',
-            location: 'sf goldengate park'
+            location: 'SF Golden Gate Park',
+            clue: 'I am so hot I am able to attain many beauties under my roof. <br>A location inside golden gate [Ref: <a href="https://goldengatepark.com/wp-content/uploads/2011/03/map_of_golden_gate_park.pdf" target="_blank">Golden Gate Park Map</a>]<br>'
         },
         cta: '/pages/book/detail.html?id=3',
         imgs: {
@@ -84,6 +85,7 @@ var il2293 = (function(){
                     $('.hero .heading1').html(infoContent.hero.heading);
                     $('.hero .description').html(infoContent.hero.desc);
                     $('.content .description').html(infoContent.content.desc);
+                    $('.content .clue').html(infoContent.content.clue);
                     $('.content .difficulty').html(infoContent.content.difficulty);
                     $('.content .player').html(infoContent.content.player);
                     $('.content .team').html(infoContent.content.team);
@@ -97,6 +99,12 @@ var il2293 = (function(){
                     $('#look-around img').on('click',function(){
                         var imgId = $(this).data('id');
                         $('.modal-body img').attr('src',infoContent.imgs[imgId]);
+                    })
+
+                    $('.il2293-info .content .ans-link').on('click',function(e){
+                        e.preventDefault();
+                        $('.ans').css('display','inline-block');
+                        $('.ans').text('Conservatory of Flowers')
                     })
 
                 }else{
